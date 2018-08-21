a690df3aa86c03174b9cebdbc54b0885054115eb
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2018-08-21 15:53:25 +1000

    Finishing height:: hair :: and sex :: functions and test with one example

--- Proj1.hs
+++ Proj1.hs
@@ -1,6 +1,8 @@
 --module Proj1 (Person, parsePerson, height, hair, sex,
   --            GameState, initialGuess, nextGuess, feedback) where
 import Data.List
+import Data.Maybe
+
 
 type Height = Char
 type HairColor = Char
@@ -14,15 +16,28 @@ data GameState = GameState [Person]
 
 
 main = do 
+    let person = (parsePerson "ABC")
     print (1)
     print (parsePerson "abc")
+    print (hair (fromJust person))
 
+{-
+takes a three-character string and returns Just p, where p is the person specified by that string. 
+If an invalid string is provided, returns Nothing.
+-}
 parsePerson :: String -> Maybe Person 
 parsePerson [] = Nothing
 parsePerson (h:hc:s:[]) = Just (Person [h:hc:s:[]])
 
-{-height :: Person -> Char
-height (h:hc:s:[]) = h-}
+
+height :: Person -> Char
+height (Person [h:hc:s:[]]) = h 
+
+hair :: Person -> Char 
+hair (Person [h:hc:s:[]]) = hc
+
+sex :: Person -> Char 
+sex (Person [h:hc:s:[]]) = s
 
 {-
 initialGuess :: ([Person], GameState)
