be0346112ed51ee6712f92eea252746b7650f1ea
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2018-08-24 12:53:34 +1000

    Totally change. The initial guess now has the functionality of performing the reduction

--- Proj1.hs
+++ Proj1.hs
@@ -2,24 +2,19 @@
   --            GameState, initialGuess, nextGuess, feedback) where
 import Data.List
 import Data.Maybe
-
+import Debug.Trace
 
 type Height = Char
 type HairColor = Char
 type Sex = Char
-data Person = Person [String]
-        deriving Show
+type Person = [String]
+type State = [Person]
 
 type Score = (Int, Int, Int, Int)
 
 data GameState = GameState [Person]
+        deriving (Show,Eq,Ord)
 
-
-main = do 
-    let person = (parsePerson "ABC")
-    print (1)
-    print (parsePerson "abc")
-    print (hair (fromJust person))
 
 {-
 takes a three-character string and returns Just p, where p is the person specified by that string. 
@@ -27,22 +22,39 @@ If an invalid string is provided, returns Nothing.
 -}
 parsePerson :: String -> Maybe Person 
 parsePerson [] = Nothing
-parsePerson (h:hc:s:[]) = Just (Person [h:hc:s:[]])
+parsePerson (h:hc:s:[]) = Just [[h,hc,s]]
 
 
 height :: Person -> Char
-height (Person [h:hc:s:[]]) = h 
+height ([h:hc:s:[]]) = h 
 
 hair :: Person -> Char 
-hair (Person [h:hc:s:[]]) = hc
+hair ([h:hc:s:[]]) = hc
 
 sex :: Person -> Char 
-sex (Person [h:hc:s:[]]) = s
+sex ([h:hc:s:[]]) = s
 
-{-
-initialGuess :: ([Person], GameState)
+
+initialGuess :: ([Person], State)
 initialGuess = (guess, state)
-         where 
+         where     
+          allPeople= [ [h,hc,s] | h<-["S","T"], hc<- ["B", "R","D"], s<-["M","F"] ] 
+          --  如果是 allPeople = Person ([[h,hc,s]| h<-['S','T'], hc<- ['B','R','D'], s<-['M','F']])
+          --  这样写就不行。很奇怪。。没法消除掉这个guess
+          guess = [["S","B","M"]]
+            --["SBM"]
+          state =allPeople \\ guess
+          -- allPeople :: Person 
+            {-allPeople = Person ([[h,hc,s]| h<-['S','T'], hc<- ['B','R','D'], s<-['M','F']])
+            onePossible = Person ["SBM"]
+            guess = [OnePossible]    -- guess :: [Person]
+            state = GameState ([allPeople] \\ [Person ["SBF"]])     -- GameState needs a list of Person. -}
             
-            guess = Person "SBM"-}
-              
+
+main = do 
+    let person = (parsePerson "ABC")
+    --let (people, gamestate, allPeople) = initialGuess
+    print (1)
+    print (parsePerson "abc")
+    print (hair (fromJust person))
+    print (initialGuess )
