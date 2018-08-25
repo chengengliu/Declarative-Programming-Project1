599507d8eba36a07609b83c608e71fcb117cf672
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2018-08-25 15:17:14 +1000

    Finish rewriting the type for Person

--- Proj1.hs
+++ Proj1.hs
@@ -7,7 +7,7 @@ import Debug.Trace
 type Height = Char
 type HairColor = Char
 type Sex = Char
-type Person = [String]
+type Person = String  -- Deceide to treat Person as a list of char[Char]. Rather than list of list of char.([String])
 type GameState = [Person]
 
 type Score = (Int, Int, Int, Int)
@@ -21,25 +21,25 @@ If an invalid string is provided, returns Nothing.
 -}
 parsePerson :: String -> Maybe Person 
 parsePerson [] = Nothing
-parsePerson (h:hc:s:[]) = Just [[h,hc,s]]
+parsePerson (h:hc:s:[]) = Just [h,hc,s]
 
 
 height :: Person -> Char
-height ([h:hc:s:[]]) = h 
+height (h:hc:s:[]) = h 
 
 hair :: Person -> Char 
-hair ([h:hc:s:[]]) = hc
+hair (h:hc:s:[]) = hc
 
 sex :: Person -> Char 
-sex ([h:hc:s:[]]) = s
+sex (h:hc:s:[]) = s
 
 
 initialGuess :: ([Person], GameState)
-initialGuess = ([guess], state)
+initialGuess = (guess, state)
          where     
           allPeople= [ [h,hc,s] | h<-['S','T'], hc<- ['B','R','D'], s<-['M','F'] ] 
           guess = ["SBM"]
-          state = [allPeople \\ guess]
+          state = allPeople \\ guess
 
           -- 夜访吸血鬼
           -- allPeople :: Person 
@@ -85,7 +85,7 @@ feedback culprits lineups =
 -- If and only if the nth element in two lists are the same. 
   -- Helper function 
 areTheSame :: Int -> Person -> Person -> Bool
-areTheSame n first second  =  (first !! n!!n) == (second !! n !!n)
+areTheSame n first second  =  (first !! n) == (second !! n )
 -- 我知道了 问题出在了对 areTheSame的这里。 判断有问题。
 -- input的type是 [[[Char]]], 也就是list of list of string，三层的char。这里areTheSame比较的
 -- 是以为个人为单位的相同。而我们这里需要的是以字母为单位的比较的函数。 
@@ -100,14 +100,14 @@ main = do
 
 
     -- Unit test for feedback  
-    let c1 = [["SBM"], ["SBF"]] 
-        l1 = [["SBM"], ["TRF"]]
-        c2 = [["SBM"], ["SBF"]]
-        l2 = [["TRF"], ["SBF"]]
-        c3 = [["SBM"], ["SBF"]]
-        l3 = [["TRF"], ["TRM"]]
-        c4 = [["SBM"], ["TDf"]]
-        l4 = [["TRF"], ["SDM"]]
+    let c1 = ["SBM", "SBF"]
+        l1 = ["SBM", "TRF"]
+        c2 = ["SBM", "SBF"]
+        l2 = ["TRF", "SBF"]
+        c3 = ["SBM", "SBF"]
+        l3 = ["TRF", "TRM"]
+        c4 = ["SBM", "TDf"]
+        l4 = ["TRF", "SDM"]
     print (feedback c1 l1)
     print $ feedback c2 l2
     print $ feedback c3 l3
