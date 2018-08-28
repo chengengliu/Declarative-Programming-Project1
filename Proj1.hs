297307b5f2b1d8f4481072b5e56accfd74272b9e
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2018-08-28 12:26:44 +1000

    Add comments

--- Proj1.hs
+++ Proj1.hs
@@ -48,15 +48,13 @@ feedback culprits lineups =
       (height, hairColor, sex) = (0,1,2)
       -- The deleteFirstsBy function takes a predicate and two lists and returns the first list with 
       -- the first occurrence of each element of the second list removed.
-      correctHeight =  totalNumber -
-        length (deleteFirstsBy (areTheSame height) culprits lineups)-
-        totalCorrect
-      correctColor = totalNumber -
-        length (deleteFirstsBy (areTheSame hairColor) culprits lineups)-
-        totalCorrect
-      correctSex = totalNumber - 
-        length (deleteFirstsBy (areTheSame sex) culprits lineups)-
-        totalCorrect
+      correctHeight =  totalNumber - totalCorrect-
+        length (deleteFirstsBy (areTheSame height) culprits lineups) 
+      correctColor = totalNumber - totalCorrect-
+        length (deleteFirstsBy (areTheSame hairColor) culprits lineups)
+      correctSex = totalNumber - totalCorrect- 
+        length (deleteFirstsBy (areTheSame sex) culprits lineups)
+        
 
 -- Given two lists and the postion we are interested in, return true
 -- If and only if the nth element in two lists are the same. 
@@ -69,7 +67,7 @@ areTheSame n first second  =  (first !! n) == (second !! n )
 -- Person:: [Char]
 -- [Person] :: [[Char]]
 -- GameState :: [Person]  == [[Char]]
-nextGuess :: ([Person],GameState) -> (Int,Int,Int,Int) -> ([Person],GameState)
+nextGuess :: ([Person],GameState) -> Score -> ([Person],GameState)
 nextGuess (lastGuess, state) score = (newGuess, newState)
   where 
     -- Cut the state such that only the guesses that 
@@ -78,29 +76,34 @@ nextGuess (lastGuess, state) score = (newGuess, newState)
       feedback candidate lastGuess == score] 
     newGuess  = selectGuess newState
 
-
+-- For each possible guesses in the remaining state, calculate the utility score 
+-- for the guess. Then based on the effeciency performance of each guess, select
+-- the most effect one ( with the most minimum possible candidates remaining. )
 selectGuess :: GameState -> Guess 
-selectGuess state = fst (head guess)
+selectGuess state = fst (head guess) -- The first one is the most effective. 
   where 
     candidates = [(goal, utilityScore)
       | goal <- state, 
       let currentState = state \\ [goal],
       let utilityScore = utilityState goal currentState]
-    guess = sortBy(compare `on` snd ) candidates -- Sort all remaining choices based on the effeciency. 
+    guess = sortBy(compare `on` snd ) candidates 
+    -- Sort all remaining choices based on the effeciency. 
 
--- Get a utility function to judge the effectness of a guess by calculating
--- the remaining percentage of possible guesses. 
--- The samll number is, the better efficiency it is. 
-
-
-utilityState ::  Guess -> GameState -> (Double, [Score]) 
+    
+-- Simply compute compute the average number of possible lineups that will 
+-- remain after each lineup by grouping guesses with the same feedback(scores).
+-- The smaller the number is, the more efficient the reduction is. 
+-- totslScores is [Score] after sorting and grouping, the number of the 
+-- same score will be calculated. 
+utilityState ::  Guess -> GameState -> Double 
 utilityState possibleSelection states = 
-  (sum [ (eachPossible/ totalCombinations)*eachPossible
-    | g <-groupLineups, let eachPossible = (fromIntegral . length) g], scores)
+  sum [ (eachPossNumber/ totalCombinations)*eachPossNumber
+    | g <-groupLineups, let eachPossNumber = (fromIntegral . length) g]
   where
-    scores =  [score | guess <- states, let score = feedback possibleSelection guess]
-    groupLineups = (group.sort) scores   
-    totalCombinations = (fromIntegral . length) scores
+    totalScores =  [score | guess <- states, 
+      let score = feedback possibleSelection guess]
+    groupLineups = (group.sort) totalScores   
+    totalCombinations = (fromIntegral . length) totalScores
     
 
 
